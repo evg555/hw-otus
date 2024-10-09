b4b62b8a6420e7d063af3480122d45b4f3eb5d6f
Evgeniy Krylov <ontropos@mail.ru>
2024-10-09 12:31:34 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,46 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	var (
+		sb         strings.Builder
+		prevLetter string
+		isPrevNum  bool
+	)
+
+	for i, letter := range str {
+		num, err := strconv.Atoi(string(letter))
+
+		if i == 0 && err == nil {
+			return "", ErrInvalidString
+		}
+
+		if err == nil {
+			if isPrevNum {
+				return "", ErrInvalidString
+			}
+
+			sb.WriteString(strings.Repeat(prevLetter, num))
+			isPrevNum = true
+		} else {
+			if !isPrevNum {
+				sb.WriteString(prevLetter)
+			}
+
+			prevLetter = string(letter)
+			isPrevNum = false
+		}
+	}
+
+	if !isPrevNum {
+		sb.WriteString(prevLetter)
+	}
+
+	return sb.String(), nil
 }
