ed7e61f532d6445e8f956970f0774ecb006ed59f
Evgeniy Krylov <ontropos@mail.ru>
2024-11-17 01:07:17 +0300

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,67 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3" //nolint:depguard
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrIsDirectory           = errors.New("it is a directory")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	src, err := os.OpenFile(fromPath, os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	srcInfo, err := src.Stat()
+	if err != nil {
+		return err
+	}
+
+	if srcInfo.IsDir() {
+		return ErrIsDirectory
+	}
+
+	if srcInfo.Size() == 0 || toPath == "" {
+		return ErrUnsupportedFile
+	}
+
+	if offset > srcInfo.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	dst, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	if offset > 0 {
+		_, err = src.Seek(offset, io.SeekStart)
+		if err != nil {
+			return err
+		}
+	}
+
+	if limit == 0 || limit+offset > srcInfo.Size() {
+		limit = srcInfo.Size() - offset
+	}
+
+	bar := pb.Full.Start64(limit)
+	barReader := bar.NewProxyReader(src)
+	defer barReader.Close()
+
+	_, err = io.CopyN(dst, barReader, limit)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,202 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	fileDir           = "testdata/"
+	inputFile         = "input.txt"
+	outputFile        = "out.txt"
+	notDefinedLenFile = "/dev/urandom"
+	notExistFile      = "not_exist"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("full copying", func(t *testing.T) {
+		out, err := os.CreateTemp(fileDir, outputFile)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		defer os.Remove(out.Name())
+
+		err = Copy(fileDir+inputFile, out.Name(), 0, 0)
+		require.Nil(t, err)
+		require.FileExists(t, out.Name())
+
+		inputInfo, _ := os.Stat(fileDir + inputFile)
+		outInfo, _ := out.Stat()
+
+		require.Equal(t, inputInfo.Size(), outInfo.Size())
+	})
+
+	t.Run("copying with limit less than len of file", func(t *testing.T) {
+		limit = 1000
+
+		out, err := os.CreateTemp(fileDir, outputFile)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		defer os.Remove(out.Name())
+
+		err = Copy(fileDir+inputFile, out.Name(), 0, limit)
+		require.Nil(t, err)
+		require.FileExists(t, out.Name())
+
+		outInfo, _ := out.Stat()
+
+		require.LessOrEqual(t, limit, outInfo.Size())
+	})
+
+	t.Run("copying with limit more than len of file", func(t *testing.T) {
+		limit = 10000
+
+		out, err := os.CreateTemp(fileDir, outputFile)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		defer os.Remove(out.Name())
+
+		err = Copy(fileDir+inputFile, out.Name(), 0, limit)
+		require.Nil(t, err)
+		require.FileExists(t, out.Name())
+
+		inputInfo, _ := os.Stat(fileDir + inputFile)
+		outInfo, _ := out.Stat()
+
+		require.Equal(t, inputInfo.Size(), outInfo.Size())
+	})
+
+	t.Run("copying with offset", func(t *testing.T) {
+		offset = 1000
+
+		out, err := os.CreateTemp(fileDir, outputFile)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		defer os.Remove(out.Name())
+
+		err = Copy(fileDir+inputFile, out.Name(), offset, 0)
+		require.Nil(t, err)
+		require.FileExists(t, out.Name())
+
+		inputInfo, _ := os.Stat(fileDir + inputFile)
+		outInfo, _ := out.Stat()
+
+		require.Equal(t, inputInfo.Size()-offset, outInfo.Size())
+	})
+
+	t.Run("copying with offset and limit", func(t *testing.T) {
+		offset = 100
+		limit = 1000
+
+		out, err := os.CreateTemp(fileDir, outputFile)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		defer os.Remove(out.Name())
+
+		err = Copy(fileDir+inputFile, out.Name(), offset, limit)
+		require.Nil(t, err)
+		require.FileExists(t, out.Name())
+
+		outInfo, _ := out.Stat()
+
+		require.Equal(t, limit, outInfo.Size())
+	})
+
+	t.Run("copying with offset and limit more than len of file", func(t *testing.T) {
+		offset = 6000
+		limit = 1000
+
+		out, err := os.CreateTemp(fileDir, outputFile)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		defer os.Remove(out.Name())
+
+		err = Copy(fileDir+inputFile, out.Name(), offset, limit)
+		require.Nil(t, err)
+		require.FileExists(t, out.Name())
+
+		inInfo, _ := os.Stat(fileDir + inputFile)
+		outInfo, _ := out.Stat()
+
+		fmt.Println(inInfo.Size() - offset)
+
+		require.Equal(t, inInfo.Size()-offset, outInfo.Size())
+	})
+
+	t.Run("overwriting existed out file", func(t *testing.T) {
+		limit = 10
+
+		out, err := os.CreateTemp(fileDir, outputFile)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		defer os.Remove(out.Name())
+
+		err = Copy(fileDir+inputFile, out.Name(), 0, 0)
+		require.Nil(t, err)
+		require.FileExists(t, out.Name())
+
+		inputInfo, _ := os.Stat(fileDir + inputFile)
+		outInfo, _ := out.Stat()
+
+		require.Equal(t, inputInfo.Size(), outInfo.Size())
+
+		err = Copy(fileDir+inputFile, out.Name(), 0, limit)
+		require.Nil(t, err)
+		require.FileExists(t, out.Name())
+
+		outInfo, _ = out.Stat()
+
+		require.Equal(t, limit, outInfo.Size())
+	})
+}
+
+func TestCopyError(t *testing.T) {
+	t.Run("got directory instead file", func(t *testing.T) {
+		err := Copy(fileDir, "", 0, 0)
+		require.Error(t, err)
+		require.ErrorIs(t, err, ErrIsDirectory)
+	})
+
+	t.Run("file not exist", func(t *testing.T) {
+		err := Copy(fileDir+notExistFile, "", 0, 0)
+		require.Error(t, err)
+		require.ErrorIs(t, err, fs.ErrNotExist)
+	})
+
+	t.Run("unsupported file", func(t *testing.T) {
+		err := Copy(notDefinedLenFile, "", 0, 0)
+		require.Error(t, err)
+		require.ErrorIs(t, err, ErrUnsupportedFile)
+	})
+
+	t.Run("out file not defined", func(t *testing.T) {
+		err := Copy(fileDir+inputFile, "", 0, 0)
+		require.Error(t, err)
+		require.ErrorIs(t, err, ErrUnsupportedFile)
+	})
+
+	t.Run("offset more than len of file", func(t *testing.T) {
+		offset = 7000
+
+		err := Copy(fileDir+inputFile, outputFile, offset, 0)
+		require.Error(t, err)
+		require.ErrorIs(t, err, ErrOffsetExceedsFileSize)
+	})
 }
