96d48c75f5dc8aed419c84fff1bb564ee2ef7bf5
Evgeniy Krylov <ontropos@mail.ru>
2024-12-26 00:22:19 +0300

    HW11 is completed

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,135 @@
 package main
 
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+const (
+	defaultTimeout = 10 * time.Second
+)
+
+var (
+	ErrConnTimeout            = errors.New("connection timed out")
+	ErrReceiveTerminateSignal = errors.New("received terminate signal, connection closed")
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	var timeout time.Duration
+
+	flag.DurationVar(&timeout, "timeout", defaultTimeout, "connection timeout")
+	flag.Parse()
+
+	if len(flag.Args()) < 2 {
+		fmt.Println("Usage: go-telnet [--timeout=10s] host port")
+		os.Exit(1)
+	}
+
+	host := flag.Arg(0)
+	port := flag.Arg(1)
+	address := net.JoinHostPort(host, port)
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	ctx, stop := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	client := NewTelnetClient(address, timeout, os.Stdin, os.Stdout)
+
+	err := client.Connect()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to connect: %v\n", err)
+		return
+	}
+	defer client.Close()
+
+	fmt.Fprintf(os.Stderr, "connected to %s\n", address)
+
+	sendCh := sendRoutine(client)
+	receiveCh := receiveRoutine(client)
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	// sender
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-ctx.Done():
+				if errors.Is(ctx.Err(), context.DeadlineExceeded) {
+					fmt.Fprintln(os.Stderr, ErrConnTimeout)
+				} else {
+					fmt.Fprintln(os.Stderr, ErrReceiveTerminateSignal)
+				}
+
+				return
+			case err = <-sendCh:
+				fmt.Fprintln(os.Stderr, err)
+				client.Close()
+				return
+			}
+		}
+	}()
+
+	// receiver
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case err = <-receiveCh:
+				if !errors.Is(err, ErrConnClosed) {
+					fmt.Fprintln(os.Stderr, err)
+				}
+				return
+			}
+		}
+	}()
+
+	wg.Wait()
+}
+
+func sendRoutine(client TelnetClient) <-chan error {
+	errCh := make(chan error)
+
+	go func() {
+		defer close(errCh)
+		for {
+			err := client.Send()
+			if err != nil {
+				errCh <- err
+				return
+			}
+		}
+	}()
+
+	return errCh
+}
+
+func receiveRoutine(client TelnetClient) <-chan error {
+	errCh := make(chan error)
+
+	go func() {
+		defer close(errCh)
+		for {
+			err := client.Receive()
+			if err != nil {
+				errCh <- err
+				return
+			}
+		}
+	}()
+
+	return errCh
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"errors"
 	"io"
+	"net"
 	"time"
 )
+
+var ErrConnClosed = errors.New("connection closed by peer")
 
 type TelnetClient interface {
 	Connect() error
@@ -12,10 +16,65 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type telnetClient struct {
+	address string
+	timeout time.Duration
+	conn    net.Conn
+	in      io.ReadCloser
+	out     io.Writer
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &telnetClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+func (c *telnetClient) Connect() error {
+	conn, err := net.DialTimeout("tcp", c.address, c.timeout)
+	if err != nil {
+		return err
+	}
+
+	c.conn = conn
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *telnetClient) Close() error {
+	return c.conn.Close()
+}
+
+func (c *telnetClient) Send() error {
+	buf := make([]byte, 4096)
+
+	n, err := c.in.Read(buf)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.conn.Write(buf[:n])
+	if err != nil {
+		return ErrConnClosed
+	}
+
+	return nil
+}
+
+func (c *telnetClient) Receive() error {
+	buf := make([]byte, 4096)
+
+	n, err := c.conn.Read(buf)
+	if err != nil {
+		return ErrConnClosed
+	}
+
+	_, err = c.out.Write(buf[:n])
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
